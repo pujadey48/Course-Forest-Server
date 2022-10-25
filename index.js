3b0268ac919af368e36aa9c6f898be393d5b1806
Puja Dey <pujadey48@gmail.com>
2022-10-25 06:05:16 +0600

    server run

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+app.get("/", (req,res)=> {
+    res.send("Server is running")
+})
+
+app.listen(Port,()=>{
+    console.log('server is running', Port);
+});

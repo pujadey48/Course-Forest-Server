c5da57e2b9df046072fa038be54d81ebb23401b6
Puja Dey <pujadey48@gmail.com>
2022-10-25 08:08:56 +0600

    API create

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 
+const courses = require('./data/courses.json');
+
 app.get("/", (req,res)=> {
     res.send("Server is running")
 })
+
+app.get('/allCourses',(req, res) =>{
+    res.send(courses);
+});
+
+app.get('/courses/:id',(req, res) =>{
+    const id = req.params.id;
+    const course = courses.find(n => n._id === id);
+    res.send(course);
+});
 
 app.listen(Port,()=>{
     console.log('server is running', Port);
